a98fea0c7155db174d728d389c4056e7f52e61dc
Danila Odintsov <captain-kush@yandex.ru>
2021-06-12 09:40:41 +0400

    db

--- config.php
+++ config.php
@@ -1,9 +1,9 @@
 <?php 
 
-$db_host = 'danissima';
+$db_host = 'newmechizu';
 $db_user = 'root';
-$db_password = 'root';
-$db_name = 'yumechizu';
+$db_password = '';
+$db_name = 'yume_chizu';
 
 $mysqli = mysqli_connect($db_host, $db_user, $db_password, $db_name);
 $mysqli->query("SET NAMES 'UTF-8'");
